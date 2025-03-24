350681f0c0890b602f6b267822f16f6b3edaa8a4
Farhad <mhmdmhdyjlwkhany8@gmail.com>
2025-03-24 03:30:27 -0700

    add validators

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,16 +1,21 @@
 package db;
 import db.exception.EntityNotFoundException;
-
+import db.exception.InvalidEntityException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
     private static ArrayList<Entity> entities = new ArrayList<>();
     private static int number = 1;
+    private static HashMap<Integer, Validator> validators = new HashMap<>();
 
     private Database(){
     }
 
-    public static void add(Entity e){
+    public static void add(Entity e) throws InvalidEntityException {
+        Validator validator = validators.get(e.getEntityCode());
+        validator.validate(e);
+
         e.id = number;
         entities.add(e.copy());
         number += 1;
@@ -38,7 +43,10 @@ public class Database {
         }
     }
 
-    public static void update(Entity e){
+    public static void update(Entity e) throws InvalidEntityException {
+        Validator validator = validators.get(e.getEntityCode());
+        validator.validate(e);
+
         boolean notFound = true;
         for(Entity x : entities){
             if(x.id == e.id){
@@ -46,8 +54,18 @@ public class Database {
                 notFound = false;
             }
         }
+
         if(notFound){
             throw new EntityNotFoundException(e.id);
         }
     }
+
+    public static void registerValidator(int entityCode , Validator validator) {
+        if(validators.containsKey(entityCode)){
+            throw new IllegalArgumentException("This validator has already been added to the hashmap validators");
+        }
+
+        validators.put(entityCode , validator);
+    }
+
 }
